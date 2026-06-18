ebfc97ca00ac87b10cf87d1904441d3aa477dc44
Fien Spriet <fien.spriet@hogent.be>
2026-06-18 15:20:10 +0200

    Kennisclip + focusoefeningen in PDF deel 1

---
+++ h01-codeSnippetsVoorOefeningen/src/main/java/cui/Som.java
@@ -0,0 +1,11 @@
+package cui;
+
+import java.lang.*;
+
+public class Som extends Object {
+	public static void main(String[] args)	{
+		int x = 2, y = 3, som;
+		som = x * x + y * y;
+		System.out.println("Som = " + som);
+	}
+}
